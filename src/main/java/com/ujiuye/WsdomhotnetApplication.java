f40f616041806bfd4d41e4072dc49587a49e9da2
root <1820949623@qq.com>
2020-03-05 09:14:26 +0800

    架子111

--- src/main/java/com/ujiuye/WsdomhotnetApplication.java
+++ src/main/java/com/ujiuye/WsdomhotnetApplication.java
@@ -1,8 +1,9 @@
 package com.ujiuye;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.ujiuye.dao")
 @SpringBootApplication
 public class WsdomhotnetApplication {
 
